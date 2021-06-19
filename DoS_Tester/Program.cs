68368a00a3ff0bfc2bc9d94e2e0a01ed3bfb2929
Joshua Whitfield <joshwhitfield9919@gmail.com>
2021-06-19 19:37:45 +0100

    init

--- DoS_Tester/Program.cs
+++ DoS_Tester/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
 
 namespace DoS_Tester
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            for (int i = 0; i < 100; i++)
+            {
+                Start(RunPage);
+            }
+            
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        public static Thread Start(Action action)
+        {
+            Thread thread = new Thread(() => { action(); });
+            thread.Start();
+            return thread;
+        }
+
+        public static void RunPage()
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\joshua.whitfield\\source\\repos\\DoS_Tester");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                driver.Navigate().GoToUrl("http://localhost:51498/Reports/DueDiary/DueDiary.aspx");
+                while (true)
+                {
+                    try
+                    {
+                        driver.FindElement(By.Id("MainContent_btnRefreshGV")).Click();
+                        Thread.Sleep(10000);
+                    }
+                    catch (Exception e)
+                    {
+                        //ignore
+                    }
+                }
+            }).Start();
         }
     }
 }
