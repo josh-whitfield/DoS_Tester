09fc1e615bf6cfd1346de11a003ebca87f3dec07
Joshua Whitfield <joshwhitfield9919@gmail.com>
2021-06-19 20:02:45 +0100

    Made dynamic

--- DoS_Tester/Program.cs
+++ DoS_Tester/Program.cs
@@ -9,34 +9,47 @@ namespace DoS_Tester
     {
         private static void Main()
         {
-            for (int i = 0; i < 100; i++)
+            Console.WriteLine("Please enter the URL:");
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number of instances to run:");
+            int dosLimit;
+            string tmpDosLimit = Console.ReadLine();
+
+            while (!int.TryParse(tmpDosLimit, out dosLimit))
             {
-                Start(RunPage);
+                Console.WriteLine("Not a valid number, try again.");
+                tmpDosLimit = Console.ReadLine();
+            }
+            
+            for (int i = 0; i < dosLimit; i++)
+            {
+                Start(url);
             }
             
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
 
-        public static Thread Start(Action action)
+        public static Thread Start(string url)
         {
-            Thread thread = new Thread(() => { action(); });
+            Thread thread = new Thread(() => RunPage(url));
             thread.Start();
             return thread;
         }
 
-        public static void RunPage()
+        public static void RunPage(string url)
         {
-            IWebDriver driver = new ChromeDriver("C:\\Users\\joshua.whitfield\\source\\repos\\DoS_Tester");
+            IWebDriver driver = new ChromeDriver(System.Reflection.Assembly.GetExecutingAssembly().Location);
             new Thread(() =>
             {
                 Thread.CurrentThread.IsBackground = true;
-                driver.Navigate().GoToUrl("http://localhost:51498/Reports/DueDiary/DueDiary.aspx");
+                driver.Navigate().GoToUrl(url);
                 while (true)
                 {
                     try
                     {
-                        driver.FindElement(By.Id("MainContent_btnRefreshGV")).Click();
+                        driver.FindElement(By.CssSelector("[id$=refresh]")).Click();
                         Thread.Sleep(10000);
                     }
                     catch (Exception e)
